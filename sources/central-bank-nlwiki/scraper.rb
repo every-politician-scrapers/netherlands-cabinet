1104ae977ff08428e2d5c6282916fa10324e9fe0
Tony Bowden <tony@tmtm.com>
2022-01-16 07:21:04 +0200

    compare nl Central Bank Governor

---
+++ sources/central-bank-nlwiki/scraper.rb
@@ -0,0 +1,32 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Naam'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[_ start end name].freeze
+    end
+
+    def raw_end
+      super.gsub('heden', '')
+    end
+
+    def tds
+      noko.css('td,th')
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
