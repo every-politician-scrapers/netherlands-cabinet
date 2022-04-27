5e46895b1b5bb92b5482b794a374301e9b4f92dd
Tony Bowden <tony@tmtm.com>
2022-04-27 16:51:43 +0300

    compare enwiki Mayors of the Hague

---
+++ sources/mayor-hague/scraper.rb
@@ -0,0 +1,32 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Term'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[_ img name dates].freeze
+    end
+
+    def raw_combo_date
+      super.gsub(/\(.*?\)/, '').tidy
+    end
+
+    def name_node
+      name_cell.css('a').last
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
